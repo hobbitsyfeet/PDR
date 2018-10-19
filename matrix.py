90e828c47e72864fdd52cbddffbecd4c9d2e6254
rmaccrimmon <r.maccrimm@gmail.com>
2018-10-19 17:50:19 -0600

    Add singular value decomposition with numpy matrix

--- matrix.py
+++ matrix.py
@@ -1,38 +1,68 @@
+import numpy as np
 from test_doc import test_doc
 from document import Document
 
+
 class TermDocumentMatrix():
-    def __init__(self):
-        self.td_freq = []
-        self.links = []
-        self.terms = set()
+    """ Rows correspond to terms and columns to documents
+    """
+
+    def __init__(self, document_list):
+        self.document_info = [{}]
+        self.terms = []
+        
+        # Frequency matrix 
+        self.matrix = None
+        self.matrix_str = ""
+        
+        self.get_terms(document_list)
+        self.construct_matrix(document_list)
+
+        # Singular value decompisition
+        self.U, self.S, self.Vt = np.linalg.svd(self.matrix)
+        self.S = np.diag(self.S)
 
     def get_terms(self, document_list):
+        """ Fill terms array with all unique terms
+        """
+        s = set()
         for d in document_list:
-            self.terms = self.terms.union(set(d.text.split()))
+            s = s.union(set(d.text.split()))
+        self.terms = list(s)
     
-    def count_terms(self, document_list):
-        for d in document_list:
-            row = { t:0 for t in self.terms }
-            for t in d.text.split():
-                row[t] += 1
-            self.td_freq.append(row)
+    def construct_matrix(self, document_list):
+        """ Fill the (i,j)th entry with the number of occurrences of 
+            term i in document j
+        """
+        self.matrix = np.matrix([[0]*len(document_list)]*len(self.terms))
+        for i, t in enumerate(self.terms):
+            for j, d in enumerate(document_list):
+                self.matrix.A[i][j] = d.text.count(t)
+        s = ""
+        r, c = self.matrix.shape
+        for i in range(r):
+            for j in range(c):
+                s += str(self.matrix.A[i][j]) + ','
+            s = s[:-1] + ';'
+        self.matrix_str = s[:-1]
 
-    def print_all(self):
-        for s in self.terms:
-            print(s, end="\t")
-        print()
-        for row in self.td_freq:
-            for k in row:
-                print(row[k], end="\t")
-            print()
-            
+    def reduce_rank(self, k):
+        """ Find an approximation to the frequency matrix by keeping
+            only those entries in matrices U, and Vt corresponding to
+            the largest k entries in the singular value matrix. 
+        """
+        pass
+
+    def query(self, query_string, num_results):
+        """ Given a query string, return a list of num_results indicating the
+            closest matching documetns
+        """
+        pass    
+    
+    
 if __name__ == '__main__':
     doc_list = [Document(x) for x in test_doc]
-    matrix = TermDocumentMatrix()
-    matrix.get_terms(doc_list)
-    matrix.count_terms(doc_list)
-    matrix.print_all()
+    tdm = TermDocumentMatrix(doc_list)
     
     
     
