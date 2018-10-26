532f97d7336553dc23ca597779dda613b9f7ca19
Justin <lego.man.123@hotmail.com>
2018-10-25 22:29:54 -0600

    Export/import
    -Exports documents separated by '\n' for each component
    -Imports from Files if they exist
    -Import from files work almost instantly
    - Stats displayed with # of terms form # of documents

---
+++ export.py
@@ -0,0 +1,56 @@
+from document import Document
+import os
+
+#creates a path and overwrites the existing document
+def createFile(name):
+	if not os.path.isdir("./docs"):
+		os.mkdir("./docs")
+	file = open("./docs/"+name,"w")
+	file.close()
+	return file
+
+
+#takes a list of documents and prints them into a file
+def exportDocument(docs,name):
+	if not os.path.isdir("./docs"):
+		os.mkdir("./docs")
+	file = open("./docs/"+name,"w")
+
+	for eachDocument in docs:
+		file.write(eachDocument.title + "\n")
+		for word in eachDocument.text:
+			file.write(word)
+			file.write (" ")
+		file.write("\n")
+		file.write(eachDocument.link + "\n")
+	#file.close()
+
+def readDocument(name):
+	print("Reading {}...").format(name)
+	file = open("./docs/"+name,"r")
+	totalTerms = 0
+	documents = []
+
+	counter = 0
+	for line in file:
+		if counter%3 == 0:
+			title = line
+			title = title.strip('\n')
+		if counter%3 == 1:
+			text = line
+			text = text.strip('\n')
+			words = text.split()
+			totalTerms = totalTerms + len(words)
+
+		if counter%3 == 2:
+			link = line
+			link = link.strip('\n')
+			documents.append(Document(title,words,link))
+		counter = counter + 1
+
+
+	#file.close()
+	print("Finished Reading " + name)
+	print("For {} documents, {} terms were extracted").format(len(documents), totalTerms)
+
+	return documents
--- extraction.py
+++ extraction.py
@@ -1,7 +1,17 @@
 from bs4 import BeautifulSoup
 from document import Document
+import export
+import os
 
 from NLTKtext import prepare_document
+
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+
 
 #global variable of local path to python documentation
 localDocPath = "python-3.7.1rc2-docs-html/"
@@ -22,60 +32,84 @@ def extractText(link):
 	with open(localDocPath + trimSpecifier(link), 'rb') as html:
 		soupLink = BeautifulSoup (html, features = "html.parser") #go to the link and then extract text
 		for paragraphs in soupLink.find_all("p"):
-			text = text + paragraphs.get_text().encode('utf-8').strip()
+			text = text + paragraphs.get_text()
+			#text = text.
 		text = prepare_document(text)
 		#print(text)
+
 		return text
 
 #returns a list of documents for a given section, printout(T/F) will display summary of contents
 def createDocuments(section,printout=False):
+	termCounter = 0
 	documents = []
-
-	#find python language reference and grab contents
-	sectionBlock = soup.find("a", text=section) 	#look for this line in contents.html
-	referenceContent = sectionBlock.find_next_sibling() #everything within language reference
-	duplicateLink = ""	#used to test if previous link == current
-
-	#for each content, get the title, link and text
-	for element in referenceContent.find_all("a"):	# find all "a" tags within referenceContent
-
-		link = element.get("href")				# extract the link matched with title
-		title = element.text
-
-		#does link exist already - these are subtopics
-		if trimSpecifier(duplicateLink) == trimSpecifier(link):
-			continue
-		#else they are different, create document and reset link for next time
-		duplicateLink = link	#new link, used for checking if link is used
-
-		#new Link, new list of titles, new document to download
-		print("Creating new document: " + title + " From: " + link)
-
-		#extract the text from the link provided, and create document
-		doc = Document(title,extractText(link),link)
-
-		#append doc to list of documents
+	if os.path.isfile("./docs/"+section):
+		documents = export.readDocument(section)
 		if printout is True:
-			#proof what's contained in doc new
-			print("---Contents Exporting---")
-			print("Title: " + doc.title)
-			print("Link: " + doc.link)
-			print("Text: {} ... {} terms\n").format(doc.text[:5], len(doc.text))
-		documents.append(doc)
+			for doc in documents:
+				#proof what's contained in doc new
+				print("---Contents Exporting---")
+				print("Title: " + doc.title)
+				print("Link: " + doc.link)
+				print("Text: {} ... {} terms\n").format(doc.text[:5], len(doc.text))
+				termCounter = termCounter + len(doc.text)
+			print("Toal Terms: {}").format(termCounter)
+
+
+
+
+
+	else:
+		#find python language reference and grab contents
+		sectionBlock = soup.find("a", text=section) 	#look for this line in contents.html
+		referenceContent = sectionBlock.find_next_sibling() #everything within language reference
+		duplicateLink = ""	#used to test if previous link == current
+
+		#for each content, get the title, link and text
+		for element in referenceContent.find_all("a"):	# find all "a" tags within referenceContent
+
+			link = element.get("href")				# extract the link matched with title
+			title = element.text
+
+			#does link exist already - these are subtopics
+			if trimSpecifier(duplicateLink) == trimSpecifier(link):
+				continue
+			#else they are different, create document and reset link for next time
+			duplicateLink = link	#new link, used for checking if link is used
+
+			#new Link, new list of titles, new document to download
+			print("Creating new document: " + title + " From: " + link)
+
+			#extract the text from the link provided, and create document
+			doc = Document(title,extractText(link),link)
+
+			termCounter = termCounter + len(doc.text)
+			#append doc to list of documents
+			if printout is True:
+				#proof what's contained in doc new
+				print("---Contents Exporting---")
+				print("Title: " + doc.title)
+				print("Link: " + doc.link)
+				print("Text: {} ... {} terms\n").format(doc.text[:5], len(doc.text))
+				termCounter = termCounter + len(doc.text)
+				print("Toal Terms: {}").format(termCounter)
+
+			documents.append(doc)
+		export.exportDocument(documents,section)
 	return documents
 
 
 
-
+print("Starting Process")
 
 #HOW TO USE
 #create a list named documents
-#documents = []
-
+documents = []
 #Smaller dataset - recomended for testing
-#documents = createDocuments("The Python Language Reference",False)
+PLR = createDocuments("The Python Language Reference",False)
 
 #Larger dataset
-#documents.extend(createDocuments("The Python Standard Library",False))
-#for docs in documents:
-#	print(docs.link)
+PSL = createDocuments("The Python Standard Library",False)
+
+documents.extend(PLR)
+documents.extend(PSL)
