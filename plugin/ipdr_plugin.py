42753bb732e10cbcd4378a5dca316c755503cc00
Justin <hobbitsyfeet@gmail.com>
2018-11-04 08:15:53 -0700

    Display Popup from Cursor
    Displays popup for recomendation

--- plugin/ipdr_plugin.py
+++ plugin/ipdr_plugin.py
@@ -14,11 +14,17 @@ Written by:		Justin Petluk
 '''
 
 #import modules
+
+#import urllib.request,os,hashlib; h = '6f4c264a24d933ce70df5dedcf1dcaee' + 'ebe013ee18cced0ef93d5f746d80ef60'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by) 
+
+
 import sublime
 import sublime_plugin
 
+
+
 class ipdrCommand(sublime_plugin.TextCommand):
-	
+
 	def line_next(self,pos):
 		return self.view.line(pos)[1]+1
 
@@ -26,7 +32,7 @@ class ipdrCommand(sublime_plugin.TextCommand):
 		return self.view.line(pos)[0]-1
 
 	def run(self, view):
-		
+
 		#declare things
 		comment_list = []
 		comment_block = []
@@ -38,38 +44,41 @@ class ipdrCommand(sublime_plugin.TextCommand):
 		#open the file and read each line
 		with open(filename) as f:
 			lines = f.readlines()
-		
+
 		#obtain the position of the cursor, returning the line number of where
 		#the cursor is
 		pos = self.view.sel()[0].begin()
-		cursor_line = self.view.rowcol(pos)[0]+1
-		print(cursor_line)
-		print(self.view.line(pos))
+		cursor_line = self.view.rowcol(pos)[0]
+		#print(cursor_line)
+		#print(self.view.line(pos))
+		line = lines[cursor_line]
+		line = line.replace("\t", "")
 
-		#in each line, check for ''' and #
-		for line in lines:
+		if (line.find("#", 0, 1) == 0):
 			line = line.replace("\t", "")
 			line = line.replace("\n", "")
-			if (line.find("#", 0, 1) == 0):
-				comment = line[1:len(line)]
-				comment_list.append(comment)
-				
-		for line in lines:
-			if (line.find("'''") == 0 and flag == False):
-				flag = True
-			elif (line.find("'''") == 0 and flag == True):
-				flag = False
-			if flag == True:
-				line = line.replace("\'\'\'","")
-				line = line.replace("\t", "")
-				line = line.replace("\n", "")
-				if (line != ""):
-					comment_block.append(line)
+			line = line.replace("#", "")
+			self.view.show_popup(line,1, -1, 500, 200, self.view.hide_popup())
 
+		#in each line, check for ''' and #
+		#for line in lines:
+		#	line = line.replace("\t", "")
+		#	line = line.replace("\n", "")
+		#	if (line.find("#", 0, 1) == 0):
+		#		comment = line[1:len(line)]
+		#		comment_list.append(comment)
 
-		print(comment_block)
-
-		print(comment_list)
+		#for line in lines:
+		#	if (line.find("'''") == 0 and flag == False):
+		#		flag = True
+		#	elif (line.find("'''") == 0 and flag == True):
+		#		flag = False
+		#	if flag == True:
+		#		line = line.replace("\'\'\'","")
+		#		line = line.replace("\t", "")
+		#		line = line.replace("\n", "")
+		#		if (line != ""):
+		#			comment_block.append(line)
 
 #check for ''' (start of multiline comment block)
 	#continue to read lines until another ''' occurs
@@ -78,7 +87,7 @@ class ipdrCommand(sublime_plugin.TextCommand):
 #check for # (start of single line comment)
 	#save line to a string
 
-#strip comment markers and newline characters 
+#strip comment markers and newline characters
 
 #safely close file
 
