fe7dff6046e17e1d4449f5b69dcba81ba1c17e62
Lance Chisholm <lance96chisholm@gmail.com>
2018-11-22 17:14:49 -0700

    Added tasks.py

---
+++ tasks.py
@@ -0,0 +1,20 @@
+#Given two lines of space seperated integers, sum both lists together. Only unique elements in each list should be considered.
+## expecting to get set, list, or counter (collections)
+
+#Given a list of space seperated ints, find the frequency that each element apears.
+## expecting collections, counter, dictionary or count
+
+#Given a string, find the index of a query substring. Also count the number of occurences.
+##Expecting to get str
+
+#given two lists of integers, return the sum of the first half of each list added together.
+##expecting math or something else
+
+#The modules described in this chapter provide functions and classes that support a functional programming style, and general operations on callables.
+##Expecting function programming modules, because this was copy pasted from that description
+
+# A function that deals with string constants, and custom string formatting
+##expecting string
+
+#A function that sorts a iterable container in an ascending or decending order
+##Expecting to get some sort of sort, or containter
